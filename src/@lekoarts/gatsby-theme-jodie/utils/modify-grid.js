578650ec6ab4fa52fd46342dea41954c3c735989
samanthasheadavis <samantha.shea.davis@gmail.com>
2021-12-31 15:32:49 -0400

    finalizing order

--- src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
+++ src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
@@ -1,10 +1,12 @@
-import {
-    filterBySlug
-  } from "@lekoarts/gatsby-theme-jodie/src/utils/resolver-templates";
-
 const modifyGrid = (data) => {
     console.log(data);
-    return filterBySlug(data, ["/info", "/how-we-met", "/cats", "/travel", "/engagement"])
+    const weMet = data.find(x => x.slug === '/we-met');
+    const weTraveled = data.find(x => x.slug === '/we-traveled');
+    const weGotCats = data.find(x => x.slug === '/we-got-cats');
+    const weGotEngaged = data.find(x => x.slug === '/engagement');
+    const info = data.find(x => x.slug === '/info');
+    const sections = [info, weMet, weTraveled, weGotCats, weGotEngaged];
+    return sections;
 }
 
 export default modifyGrid
